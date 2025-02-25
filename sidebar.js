73546fda4a4dd2d158144cac6a3c6cccfdac9796
4saad <m.shaker.saad@gmail.com>
2025-02-25 12:38:50 +0200

    Update sidebar.js

--- sidebar.js
+++ sidebar.js
@@ -3,12 +3,7 @@ document.addEventListener("DOMContentLoaded", function() {
     const sidebar = $("<div id='sidebar' class='sidebar'></div>");
     $('.NewSidebarthree').append(sidebar);
     $('.NewSidebarthree').addClass('with-sidebar');
-    if (!isMobile()) {
-        let logoSpan = $('span[name="' + LOGO_CELL_NAME + '"]');
-        logoSpan.addClass('logo');
-        $('<div id="logo" class="logo">' + logoSpan.html() + '</div>').prependTo($('#sidebar'));
-        $('div.logo').height($('div.header').height())
-    };
+
     if ($('ul.tab-box-tabs').length > 0) {
         $('#sidebar').append('<div id="tabs" class="sidebar-tabs">')
         $('.sidebar-tabs').append($('ul.tab-box-tabs'));
