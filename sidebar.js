422c0e45942745e21a89a2d0f3b4658f58fb16bf
4saad <m.shaker.saad@gmail.com>
2025-02-25 12:33:12 +0200

    Update sidebar.js

--- sidebar.js
+++ sidebar.js
@@ -1,16 +1,22 @@
-document.addEventListener("DOMContentLoaded", function() {
-    var sidebar = document.getElementById("NewSidebarthree");
-    var toggleButton = document.createElement("button");
+renderSidebar = () => {
+    alert('inn');
+    const sidebar = $("<div id='sidebar' class='sidebar'></div>");
+    $('.NewSidebarthree').append(sidebar);
+    $('.NewSidebarthree').addClass('with-sidebar');
+    if (!isMobile()) {
+        let logoSpan = $('span[name="' + LOGO_CELL_NAME + '"]');
+        logoSpan.addClass('logo');
+        $('<div id="logo" class="logo">' + logoSpan.html() + '</div>').prependTo($('#sidebar'));
+        $('div.logo').height($('div.header').height())
+    };
+    if ($('ul.tab-box-tabs').length > 0) {
+        $('#sidebar').append('<div id="tabs" class="sidebar-tabs">')
+        $('.sidebar-tabs').append($('ul.tab-box-tabs'));
+        $('a.tab').append('<div class="sidebar-border"><span class="top"></span><span class="bottom"></span></div>');
+    }
 
-    toggleButton.innerText = "☰";
-    toggleButton.style.position = "fixed";
-    toggleButton.style.top = "10px";
-    toggleButton.style.left = "10px";
-    toggleButton.style.zIndex = "1001";
-    
-    document.body.appendChild(toggleButton);
-    
-    toggleButton.addEventListener("click", function() {
-        sidebar.classList.toggle("open");
-    });
-});
+}
+
+renderDrawer = () => {
+    renderSidebar();
+ }
